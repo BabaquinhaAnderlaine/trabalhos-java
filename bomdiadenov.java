71d25bec54197036abed71b6220380c798764bff
BabaquinhaAnderlaine <babaquinhaanderlaine@gmail.com>
2026-03-23 15:48:24 -0300

    xoxo

---
+++ bomdiadenov.java
@@ -0,0 +1,35 @@
+
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class bomdiadenov{
+    public static void  main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("digite seu nome: ");
+        
+        String nomeUser = leitor.nextLine();
+        fazerBomdia(nomeUser);
+        
+        leitor.close();
+    }
+    
+    public static void fazerBomdia(String nome){
+        double hora = LocalTime.now().getHour();
+        String saudacao;
+        
+        if(hora >= 6 && hora < 12){
+            saudacao = "bom dia";
+        }
+        else if(hora >=12 && hora < 18){
+            saudacao = "boa tarde";
+        }
+        else {
+            saudacao = "boa noite";
+        }
+        
+        
+        System.out.println(saudacao+","+nome);
+    }
+    
+}
