4d983a6e2a9a04d578675f695b69205c3af2eca8
BabaquinhaAnderlaine <babaquinhaanderlaine@gmail.com>
2026-05-06 08:40:44 -0300

    add files

---
+++ classe2/Aluno.java
@@ -0,0 +1,13 @@
+class Aluno extends Pessoa {
+    String matricula;
+
+    Aluno(String nome, String fone, String cpf, String matricula) {
+        super(nome, fone, cpf);
+        this.matricula = matricula;
+    }
+
+    void aprende() {
+        System.out.println(nome + " está aprendendo.");
+    }
+}
+
