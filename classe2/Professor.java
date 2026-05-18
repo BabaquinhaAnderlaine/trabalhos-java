44f30f4a5f52522887cecd6fbe53fe634be32aea
BabaquinhaAnderlaine <babaquinhaanderlaine@gmail.com>
2026-05-18 15:42:43 -0300

    Add files via upload

--- classe2/Professor.java
+++ classe2/Professor.java
@@ -1,12 +1,30 @@
-class Professor extends Aluno {
-    String titulo;
+public class Professor extends Aluno {
 
-    Professor(String nome, String fone, String cpf, String matricula, String titulo) {
-        super(nome, fone, cpf, matricula);
+    private String titulo;
+
+    public Professor(String nome, String fone,
+                     String cpf, String mtr,
+                     String titulo) {
+
+        super(nome, fone, cpf, mtr);
         this.titulo = titulo;
     }
 
-    void ensina() {
-        System.out.println(nome + " está ensinando.");
+    public String getTitulo() {
+        return titulo;
+    }
+
+    @Override
+    public String saudacao() {
+
+        return "Olá, sou o professor "
+                + getNome()
+                + ", título: "
+                + titulo;
+    }
+
+    public String ensina() {
+
+        return getNome() + " está ensinando.";
     }
 }
--- classe2/Responsavel.java
+++ classe2/Responsavel.java
@@ -1,8 +1,38 @@
-class Responsavel extends Pessoa {
-    Aluno[] alunos;
+public class Responsavel extends Pessoa {
 
-    Responsavel(String nome, String fone, String cpf, Aluno[] alunos) {
+    private Aluno[] alunos;
+
+    public Responsavel(String nome, String fone,
+                       String cpf, Aluno[] alunos) {
+
         super(nome, fone, cpf);
         this.alunos = alunos;
+    }
+
+    public Aluno[] getAlunos() {
+        return alunos;
+    }
+
+    @Override
+    public String saudacao() {
+
+        return "Olá, sou responsável pelos alunos.";
+    }
+
+    public String mostrarAlunos() {
+
+        String texto =
+                "Alunos do responsável "
+                + getNome()
+                + ":\n";
+
+        for (Aluno aluno : alunos) {
+
+            texto += "- "
+                    + aluno.getNome()
+                    + "\n";
+        }
+
+        return texto;
     }
 }
