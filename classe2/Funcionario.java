961e5a784d7b3af7480091658f3bc8f11152142b
BabaquinhaAnderlaine <babaquinhaanderlaine@gmail.com>
2026-05-22 10:46:17 -0300

    Refactor Funcionario class constructor and methods
    Updated constructor parameters and added setter methods for cod and funcao. Modified the saudacao method to return a welcome message.

--- classe2/Funcionario.java
+++ classe2/Funcionario.java
@@ -1,18 +1,14 @@
 public class Funcionario extends Pessoa {
-
     private String cod;
     private String funcao;
 
-    public Funcionario(String nome, String fone,
-                       String cpf, String cod,
-                       String funcao) {
-
-        super(nome, fone, cpf);
-
+    public Funcionario(String nome, String telefone, String cpf, String cod, String funcao) {
+        super(nome, telefone, cpf);
         this.cod = cod;
         this.funcao = funcao;
     }
 
+    
     public String getCod() {
         return cod;
     }
@@ -21,17 +17,17 @@ public class Funcionario extends Pessoa {
         return funcao;
     }
 
-    @Override
-    public String saudacao() {
-
-        return "Olá, sou funcionário e trabalho como "
-                + funcao;
+   
+    public void setCod(String cod) {
+        this.cod = cod;
     }
 
-    public String desempenhaFuncao() {
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
 
-        return getNome()
-                + " trabalha como "
-                + funcao;
+    @Override
+    public String Saudacao() {
+        return "Bem-vindo, funcionário!";
     }
 }
