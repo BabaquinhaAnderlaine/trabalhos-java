061a61a0772ac2ba36f3cb6f7c20c1cf3e0b7652
BabaquinhaAnderlaine <babaquinhaanderlaine@gmail.com>
2026-05-25 14:07:02 -0300

    Refactor Funcionario class constructor and methods
    Refactor Funcionario constructor and methods for clarity.

--- classe2/Funcionario.java
+++ classe2/Funcionario.java
@@ -1,14 +1,20 @@
+// FUNCIONARIO
+
 public class Funcionario extends Pessoa {
+
     private String cod;
     private String funcao;
 
-    public Funcionario(String nome, String telefone, String cpf, String cod, String funcao) {
-        super(nome, telefone, cpf);
+    public Funcionario(String nome, String fone,
+                       String cpf, String cod,
+                       String funcao) {
+
+        super(nome, fone, cpf);
+
         this.cod = cod;
         this.funcao = funcao;
     }
 
-    
     public String getCod() {
         return cod;
     }
@@ -17,17 +23,17 @@ public class Funcionario extends Pessoa {
         return funcao;
     }
 
-   
-    public void setCod(String cod) {
-        this.cod = cod;
-    }
-
-    public void setFuncao(String funcao) {
-        this.funcao = funcao;
-    }
-
     @Override
-    public String Saudacao() {
-        return "Bem-vindo, funcionário!";
+    public String saudacao() {
+
+        return "Olá, sou funcionário e trabalho como "
+                + funcao;
+    }
+
+    public String desempenhaFuncao() {
+
+        return getNome()
+                + " trabalha como "
+                + funcao;
     }
 }
